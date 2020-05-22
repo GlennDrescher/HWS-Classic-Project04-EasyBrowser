6574563963a92c7972e8e54bd415559132332f95
Glenn Drescher <GlennD@me.com>
2020-05-22 22:44:52 +0200

    Initial Commit

---
+++ HWS-Classic-Project04-EasyBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS-Classic-Project04-EasyBrowser
+//
+//  Created by Glenn Drescher on 22/05/2020.
+//  Copyright © 2020 Glenn Drescher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
