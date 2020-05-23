b0a20607646d661997433e4d032d3e2bcd2e2f6f
Glenn Drescher <GlennD@me.com>
2020-05-23 11:23:12 +0200

    open button implemented, enabling loading different websites

--- HWS-Classic-Project04-EasyBrowser/ViewController.swift
+++ HWS-Classic-Project04-EasyBrowser/ViewController.swift
@@ -21,9 +21,33 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://www.google.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page ...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Samsung.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Tesla.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Microsoft.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancle", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        print(url.absoluteString)
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 
 
